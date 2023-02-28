02f930ac8ca21fc9396faedfb8f7df467290d273
Build-A-Moat <Build-A-Moat@github.com>
2023-02-28 22:44:16 +0800

    Finish q_reverse

--- queue.c
+++ queue.c
@@ -164,7 +164,16 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *cur = head->prev->prev, *last = head->prev;
+    while (cur != head) {
+        list_move_tail(cur, head);
+        cur = last->prev;
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
